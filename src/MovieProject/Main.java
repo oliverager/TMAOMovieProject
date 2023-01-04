f6c912ef4168f6548a816ef4da05ca0d88861ed7
oliverager <114471849+oliverager@users.noreply.github.com>
2023-01-04 10:10:19 +0100

    FXML

--- src/MovieProject/Main.java
+++ src/MovieProject/Main.java
@@ -1,8 +1,21 @@
 package MovieProject;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+public class Main extends Application {
+    public static void main(String[] args) throws Exception {
+        Application.launch();
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/MainWindow.fxml"));
+        Parent root = loader.load();
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
     }
 }
 
