271fd3f4f0ca626121b509e566c17da824de17d1
Janjarn <morgre01@easv365.dk>
2023-01-09 11:39:47 +0100

    fix update

--- src/MovieProject/GUI/Controller/MainViewController.java
+++ src/MovieProject/GUI/Controller/MainViewController.java
@@ -286,6 +286,11 @@ public void playVideo(String moviePath) throws IOException {
         double rating = Double.parseDouble(JOptionPane.showInputDialog(""));
         movieModel.updateMovieRating(movie, rating);
         MovieTableView.setItems(movieModel.showList());
+        if (rating > 0 && rating < 10) {
+
+        } else {
+            informationUser("Your rating value needs to be between 1-10");
+        }
     }
 
     public void handleSearch(ActionEvent actionEvent) {
