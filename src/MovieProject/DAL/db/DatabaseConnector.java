1f172dd51c30bb41d0e1f93f916f7fb53c47bdf6
Tomas1974 <114471994+Tomas1974@users.noreply.github.com>
2023-01-11 11:33:14 +0100

    Update DatabaseConnector.java

--- src/MovieProject/DAL/db/DatabaseConnector.java
+++ src/MovieProject/DAL/db/DatabaseConnector.java
@@ -15,19 +15,12 @@ import java.util.Properties;
 public class DatabaseConnector {
 
 
-<<<<<<< Updated upstream
-=======
-//    private static final String PROP_FILE = "Data/database.settings";
 
->>>>>>> Stashed changes
     private static final String PROP_FILE = "TMAOMovieProject/Data/database.settings";
 
 
 
-<<<<<<< Updated upstream
 
-=======
->>>>>>> Stashed changes
     private SQLServerDataSource dataSource;
 
     public DatabaseConnector() throws IOException {
